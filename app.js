eb3440e25b9c353621c73b50ba00427543c533ff
yemisi123 <yo@osmosoft.com>
2018-01-04 19:38:30 +0000

    testing node server

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+var http = require('http');
+var fs = require("fs");
+
+http.createServer(function(request, response) {
+  fs.readFile("index.html", function(err, data){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+  });
+
+  fs.readFile("index.js", function(err, data){
+    response.writeHead(200, {'Content-Type': 'text/javascript'});
+    if(err){
+			response.writeHead(404);
+			response.write("Not Found!");
+		}
+		else{
+			response.writeHead(200, {'Content-Type': 'text/javascript'});
+			response.write(data);
+		}
+		response.end();
+  });
+
+		//sendFileContent(response, request.url.toString().substring(1), "text/css");
+
+
+
+
+}).listen(3000);
